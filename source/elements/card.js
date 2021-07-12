d04c3be91897e1af980c38c3568bbd47297e39c2
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:30:53 -0400

    update build

--- source/elements/card.js
+++ source/elements/card.js
@@ -1,5 +1,9 @@
+import card from '../templates/card.js';
+
 export class Card extends Quantum {
     constructor() {
         super();
     }
 }
+
+Card.define('quantum-card', card);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Card } from './elements/card.js';
-import card from './templates/card.js';
-
-Card.define('quantum-card', card);
